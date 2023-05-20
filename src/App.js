0b4025acf7a836cbdb40a9d6991d980c3d249bf8
Aakash Nair <123412529+aknair95@users.noreply.github.com>
2023-05-20 06:16:00 +0530

    product enlisting component created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ProductInfoForm from "./Components/ProductInfo/productInfoForm";
+import ProductList from "./Components/ProductList/productList";
+import { Fragment } from "react";
+import Card from "./Components/UI/card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="App">
+      <ProductInfoForm/>
+      <ProductList/>
+    </Card>
   );
 }
 
---
+++ src/Components/ProductList/productList.js
@@ -0,0 +1,24 @@
+import "./productList.css";
+import React from "react";
+
+const ProductList=() =>{
+    return (
+        <React.Fragment>
+            <h1>PRODUCTS</h1>
+            <h2>Electronic Items</h2>
+            <h2>Food Items</h2>
+            <h2>Skincare Items</h2>
+            <h2>Cosmetics Items</h2>
+            <h2>Toys Items</h2>
+            <h2>Electrical Items</h2>
+            <h2>Decoration Items</h2>
+            <h2>Tool Items</h2>
+            <h2>Miscellaneous Items</h2>
+            <h2>Kitchen Items</h2>
+            <h2>Healthcare Items</h2>
+            <h2>Furniture Items</h2> 
+        </React.Fragment>
+    )
+}
+
+export default ProductList;
